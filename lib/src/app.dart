b7788aea1a4392dd5d9b6475c86b13c5a3fde178
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-01-18 20:52:02 -0500

    structure

---
+++ lib/src/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+//con extends indicas el tipo de widget que tienes
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Center(
+      child: Text('Holaaaa '),
+    ));
+  }
+}
