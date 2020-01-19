f5f3f1e6cbd272e112bdb5ec4935212c2b5b6dba
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-01-19 17:06:02 -0500

    a kind of flexbox on flutter

--- lib/src/pages/contador_page.dart
+++ lib/src/pages/contador_page.dart
@@ -14,35 +14,49 @@ class _ContadorPageState extends State<ContadorPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('TAEKWONDO APP'),
-        centerTitle: true,
-        elevation: 5.0,
-      ),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            'Nº de Golpes',
-            style: _estiloTexto,
-          ),
-          Text(
-            '$_conteo',
-            style: _estiloTexto,
-          )
-        ],
-      )),
-      //posicion del boton
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.videogame_asset),
-        onPressed: () {
-          _conteo++;
-          //Redibuja todo el widget
-          setState(() {});
-        },
-      ),
+        appBar: AppBar(
+          title: Text('TAEKWONDO APP'),
+          centerTitle: true,
+          elevation: 5.0,
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Nº de Golpes',
+              style: _estiloTexto,
+            ),
+            Text(
+              '$_conteo',
+              style: _estiloTexto,
+            )
+          ],
+        )),
+        //posicion del boton
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: _crearBotones());
+  }
+
+  Widget _crearBotones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {},
+        ),
+        // Propiedad que sirve para generar margen a los costados
+        // SizedBox(width: 5.0),
+        FloatingActionButton(
+          child: Icon(Icons.restore),
+          onPressed: () {},
+        ),
+        FloatingActionButton(
+          child: Icon(Icons.remove),
+          onPressed: () {},
+        ),
+      ],
     );
   }
 }
